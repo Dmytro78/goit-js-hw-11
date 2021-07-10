7bfa83ad7fef2b2cd47d9e653c3c5460a21f1c85
Dmitriy Liashchenko <fttravel78@gmail.com>
2021-07-10 20:00:47 +0300

    task01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 import '../css/common.css';
+
+const refs = {
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]'),
+};
+
+const INTERVAL_DELAY = 1000;
+let intervalId = null;
+
+refs.btnStart.addEventListener('click', changeColor);
+refs.btnStop.addEventListener('click', onBtnStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColor() {
+    intervalId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, INTERVAL_DELAY);
+    refs.btnStart.disabled = true;
+}
+
+function onBtnStop() {
+    clearInterval(intervalId);
+    refs.btnStart.disabled = false;
+}
